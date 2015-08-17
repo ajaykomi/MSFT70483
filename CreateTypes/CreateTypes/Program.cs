0fddd602fdeba3b04cd04627ff1bef47174912a2
Ajay Komireddy <akomireddy@bolthouse.com>
2015-08-17 16:55:10 -0700

    Compared behaviors of Class and strructs

--- CreateTypes/CreateTypes/Program.cs
+++ CreateTypes/CreateTypes/Program.cs
@@ -9,9 +9,18 @@ namespace CreateTypes
 {
     class Program
     {
-        static void Main(string[] args)
+      public static void Main(string[] args)
         {
-            Console.WriteLine( "factorial of 4:" +Calc.factorial(5));
+          // struct is same as class but it is value type. When a struct is initialized its value will be initialized rather than just reference. 
+          // Just keep in mind that they're created on the stack and that you're not dealing with references to them but dealing directly with them. 
+          //Whenever you have a need for a type that will be used often and is mostly just a piece of data, structs might be a good option.
+
+            CalcStruct cs = new CalcStruct();
+            CalcClass cc = new CalcClass();
+            cs.A = 6;
+            cc.A = 6;
+            Console.WriteLine( "factorial of 6 in struct:" +cs.factorial(cs)); //  this gives 720
+            Console.WriteLine("factorial of 6 in class:" + cc.factorial(cc)); // This gives 120
             Console.Read();
         }
     }
---
+++ CreateTypes/CreateTypes/Structures/CalcStruct.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateTypes.Structures
+{
+   public struct CalcStruct
+    {
+       
+       int _a;
+       public int A
+       {
+           get { return _a; }
+
+           set { _a = value; }
+       }
+       
+
+        public int factorial(CalcStruct cstruct)
+        {
+            this.A = 5;
+            int result = cstruct.A;
+            
+            while (cstruct.A !=1)
+            {
+              result = result * (cstruct.A - 1);
+              cstruct.A--;
+            }
+            return result;
+        }
+    }
+}
