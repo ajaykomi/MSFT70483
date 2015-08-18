c526c5f9944757f7c4e1ad3aadc210441bc32f04
Ajay Komireddy <akomireddy@bolthouse.com>
2015-08-18 11:39:22 -0700

    Added Enum types

---
+++ CreateTypes/CreateTypes/Enums/Timezones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateTypes.Enums
+{
+   
+   public class Timezones
+    {
+       [Flags]
+       public enum MyTimezone
+       {
+           GMT,
+           EST,
+           CST,
+           PST,
+           IST
+       }        
+    }
+}
