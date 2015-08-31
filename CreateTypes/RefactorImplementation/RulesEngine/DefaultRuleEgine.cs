5b02f8e35f3bccd72086da49e4680c170cae8551
Ajay <ajayreddy.402@gmail.com>
2015-08-31 06:45:37 -0700

    Cntd Rule Engine

--- CreateTypes/RefactorImplementation/RulesEngine/DefaultRuleEgine.cs
+++ CreateTypes/RefactorImplementation/RulesEngine/DefaultRuleEgine.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using RefactorImplementation.Rules;
 
 namespace RefactorImplementation.RulesEngine
 {
-    public class DefaultRuleEgine
+    public class DefaultRuleEgine<T> : RuleEngineBase<T>, IRuleEngine<T>
     {
+        public List<BrokenRule> Validate(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void BuildRuleSet()
+        {
+            Type valType = typeof(T);
+            PropertyInfo[] valprops = valType.GetProperties();
+            foreach (PropertyInfo valprop in valprops)
+            {
+                var ruleset = valprop.GetCustomAttributes(typeof(ValidationAtrribute), true);
+            }
+           //throw new NotImplementedException();
+        }
     }
 }
