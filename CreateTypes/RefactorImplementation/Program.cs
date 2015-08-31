6420b148c706c663c6cd6bd9d0eb91db0d0e1fc3
Ajay Komireddy <ajajk@bolthouse.com>
2015-08-31 10:13:41 -0700

    Nothing to commit

--- CreateTypes/RefactorImplementation/Program.cs
+++ CreateTypes/RefactorImplementation/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using Encapsulation.Classes;
+using System.Drawing;
+
 
 namespace RefactorImplementation
 {
@@ -10,6 +14,27 @@ namespace RefactorImplementation
     {
         static void Main(string[] args)
         {
+            Type t = typeof(Blogs);
+            Blogs firstblog = new Blogs();
+            string firstbyte = "just checking";
+
+            Console.WriteLine(t);
+            PropertyInfo[] allprops =  t.GetProperties();
+
+            foreach (PropertyInfo p in allprops)
+            {
+                Console.WriteLine(p);
+            }
+             MethodInfo[] allmethods =  t.GetMethods();
+             foreach (MethodInfo m in allmethods)
+             {
+                 Console.WriteLine(m);
+             }
+            
+            byte[] imagebytes = (byte[])(new ImageConverter().ConvertTo(firstbyte , typeof(byte)));
+
+            Console.WriteLine(imagebytes);
         }
+
     }
 }
